cf6fb341492fa654897c68da8398683dc6ab17a1
flp3 <florian.pignol2@gmail.com>
2023-04-10 16:41:27 -0600

    creating a setup file to add the dependencies

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(     
+    name='Simulator of Optical Quality',
+    author='Florian Pignol',     
+    version='0.1',     
+    install_requires=[         
+        'opencv-contrib-python',         
+        'numpy',
+        'matplotlib',
+    ]
+)
