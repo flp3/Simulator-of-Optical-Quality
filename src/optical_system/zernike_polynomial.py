c78d5f1ce7ae389d642debdb479d536fc7c9615d
flp3 <florian.pignol2@gmail.com>
2023-04-13 12:47:54 -0600

    add type hint and docstring to Zernike, plus test

--- src/optical_system/zernike_polynomial.py
+++ src/optical_system/zernike_polynomial.py
@@ -1,6 +1,9 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import typing
+import numpy.typing as npt
+
 
 
 class ZernikeIndexError(Exception):
@@ -8,24 +11,42 @@ class ZernikeIndexError(Exception):
 
 
 class ZernikeIndex():
-    def __init__(self, index):
+    def __init__(self, index: typing.Union[tuple[int, float], int]):
+        """
+        Zernike polynomials are used to analytically represent the shape and local deformations of optical surfaces or wavefronts.
+        Many instruments and commercial softwares had been using them such as Zemax.
+        However, due to many practical and computational reasons, they have slightly different structures and orderings.
+        Here we are considering the Fringe representation, from the university of Arizona.
+        Principle usage: from a double index, i.e. radius and azimuth, obtain a unique Fringe index. and reciprocally.
+
+        Args:
+            index (tuple or int): if a tuple, then we use the radial and azimuthal representation, 
+            if a unique int, then we use Fringe representation.
+        """
         if isinstance(index, tuple):
             assert index[0] >= 0
             assert abs(index[1]) <= index[0]
-            self.radius = index[0]
-            self.azimuth = index[1]
-            self.fringe = self.get_fringe
+            self.radius: int = index[0]
+            self.azimuth: float = index[1]
+            self.fringe: float = self.get_fringe
         if isinstance(index, int):
             self.fringe = index
             self.radius, self.azimuth = self.get_radius_azimuth_index
 
     @property
-    def get_fringe(self):
-        return (1 + (self.radius + np.abs(self.azimuth)) / 2) ** 2 -\
+    def get_fringe(self) -> float:
+        """
+        From radial and azimuthal representation to Fringe one
+        """
+        fringe_index: float = (1 + (self.radius + np.abs(self.azimuth)) / 2) ** 2 -\
                2 * np.abs(self.azimuth) + (1 - math.copysign(1, self.azimuth)) / 2
+        return fringe_index
 
     @property
-    def get_radius_azimuth_index(self):
+    def get_radius_azimuth_index(self) -> tuple[int, float]:
+        """
+        From Fringe representation to radial and azimuthal one.
+        """
         index_catalog = {}
         for n in range(10):
             for l in range(-n, n + 1):
@@ -40,24 +61,53 @@ class ZernikeIndex():
 
 
 class ZernikeCoefficient(ZernikeIndex):
-    def __init__(self, index, name, normalization):
+    def __init__(self,
+                 index: typing.Union[tuple[int, float], int],
+                 name: str,
+                 normalization: float):
+        """
+        The mathematical Zernike Polynomial were originally described by Frits Zernike in 1934. in
+        Zernike F, "Beugungstheorie des Schneidenver-Fahrens und Seiner Verbesserten Form der Phasenkontrastmethode", Physica, 1(1934)689–704; doi.org/10.1016/S0031-8914(34)80259-5.
+        And were developed to describe the diffracted wavefront in phase contrast imaging, in
+        Zernike F, The Diffraction Theory of Aberrations, in Optical Image Evaluation Circular 526, ( National Bureau of Standards , Washington, D. C), 1952.
+        Moreover, Zernike won the 1953 Nobel Prize in Physics for developing Phase Contrast Microscopy.
+        
+        Their main application is for optical design and testing, describing complex shapes such as freeform surfaces and fabrication errors.
+        Zernike polynomials have nice mathematical properties, indeed, they are orthogonal over the continuous unit circle. 
+        Meaning They can can represent arbitrarily complex continuous surfaces given enough terms.
+        Furthermore, their first terms efficiently represent common errors (e.g. coma, spherical aberration) seen in optics. 
+
+        Args:
+            index (tuple or int): if a tuple, then we use the radial and azimuthal representation, 
+            if a unique int, then we use Fringe representation.
+            name (string): several Zernike terms (or coefficients) represent common optical aberration, such as coma, astigmatism, defocus...
+            normalization (float): We may want to normalize the Zernike coefficients, considering the L2 norm on the unit disk should be equal to pi.
+            (integral over rho[0, 1] and theta[0, 2*pi] of (Z**2)*rho = pi)
+        """
         super().__init__(index)
-        self.name = name
-        self.normalization = normalization
+        self.name: str = name
+        self.normalization: float = normalization
         self.max_rho = 1
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return 'ZernikeCoefficient(fringe_index= %s, radius= %s, azimuth= %s, ' \
                'name = %s, normalization factor= %s' % (self.fringe, self.radius, self.azimuth, self.name,
                                                         self.normalization)
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ZernikeCoefficient):
+            return NotImplemented
         return (self.fringe == other.fringe) & (self.name == other.name) & (self.normalization == other.normalization)
 
-    def calculate_radial_polynomial(self, rho):
+    def calculate_radial_polynomial(self, rho: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
         """
-        :param rho: 2d matrix
-        :return:
+        This method calculate the Zernike radial component following the ISO 24157 Zernikes standard, on a rectangular grid.
+        
+        Args:
+            rho: 2d matrix. In order to evaluate the zernike radial component on a disk,
+            we consider rho as a sqrt(x**2 + y**2) on a rectangular grid and than set everything to 0 above the max_rho, which represent the disk radius, usually 1.
+        Returns:
+            radial_polynomial(2D matrix): The Zernike radial component on a rectangular grid.
         """
         if (self.radius - abs(self.azimuth)) % 2 != 0:
             if self.fringe == 1:
@@ -80,20 +130,37 @@ class ZernikeCoefficient(ZernikeIndex):
         radial_polynomial[rho > self.max_rho] = 0
         return radial_polynomial
 
-    def calculate_zernike_polynomial(self, rho, theta):
+    def calculate_zernike_polynomial(self, rho: npt.NDArray[np.float64], theta: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        """
+        This method calculate the Zernike coefficient following the ISO 24157 Zernikes standard, on a rectangular grid.
+        
+        Args:
+            rho (npt.NDArray[np.float64]): 2d matrix. In order to evaluate the zernike coefficient on a disk,
+            we consider rho as a sqrt(x**2 + y**2) on a rectangular quartesian grid and than set everything to 0 above the max_rho,
+            which represent the disk radius, usually 1.
+            theta (npt.NDArray[np.float64]): 2d matrix. In order to evaluate the zernike coefficient on a disk,
+            we consider theta as a arctan(y, x) on a rectangular quartesian grid
+            
+            Note: self.normalization (float): We may want to normalize the Zernike coefficients, considering the L2 norm on the unit disk should be equal to pi.
+            (integral over rho[0, 1] and theta[0, 2*pi] of (Z**2)*rho = pi)
+
+        Returns:
+            zernike_coefficient (npt.NDArray[np.float64])
+        """
         radial_polynomial = self.calculate_radial_polynomial(rho)
         if self.azimuth >= 0:
-            return radial_polynomial * np.cos(self.azimuth * theta) / self.normalization
+            zernike_coefficient: npt.NDArray[np.float64] = radial_polynomial * np.cos(self.azimuth * theta) * self.normalization
         if self.azimuth < 0:
-            return radial_polynomial * np.sin(self.azimuth * theta) / self.normalization
+            zernike_coefficient = radial_polynomial * np.sin(self.azimuth * theta) * self.normalization
+        return zernike_coefficient
 
-    def cartesian_matrix(self, N=100):
-        '''Evaluate the zernikes on a rectangular grid, and return the X, Y grid and the values'''
+    def cartesian_matrix(self, N: int=100) -> tuple[ npt.NDArray[np.float64],  npt.NDArray[np.float64]]:
+        '''In order to evaluate the zernikes on a rectangular grid, we create the quartesian X, Y grid and return the Zernike coefficient'''
         x = np.linspace(-self.max_rho, self.max_rho, N)
         [x2, y2] = np.meshgrid(x, x)
         return x, self.eval_cartesian(x2, y2)
 
-    def eval_cartesian(self, x, y):
+    def eval_cartesian(self, x: npt.NDArray[np.float64], y: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
         '''Evaluate the zernikes on predefined cartesian points'''
         rho = np.sqrt(x**2 + y**2)
         theta = np.arctan2(y, x)
@@ -105,30 +172,44 @@ class ZernikeCoefficientsError(Exception):
 
 
 class ZernikeCoefficients():
-    """
-    Return a set of Zernike Polynomials Coefficient
-
-    The particular set we are using here is the "Fringe" (or "University of Arizona"), and are
-    NOT orthonormal. They are normalized to be one at radius=1.
-    """
-
-    def __init__(self, order):
+    def __init__(self, order: int):
+        """Class that handles a set of Zernike Polynomials Coefficients using the Fringe representation.
+        
+        Args:
+            order (int): From the self.max_fringe_order, consider the first {order} coefficients.
+        """
         if order > self.max_fringe_order:
             raise ZernikeCoefficientsError('order tasked is too high.'
                                            'Maximum supported order is %s and got %s' % (self.max_fringe_order, order))
-        self.order = order
-        self.coefficients = self.get_supported_zernike[:self.order]
+        self.order: int = order
+        self.coefficients: tuple[ZernikeCoefficient, ...] = self.get_supported_zernike[:self.order]
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: typing.Union[tuple[int, float], int]) -> ZernikeCoefficient:
+        output = None
         for coef in self.coefficients:
             if isinstance(item, tuple):
                 if (coef.radius, coef.azimuth) == item:
-                    return coef
+                    output = coef
+                    break
             else:
                 if coef.fringe == item:
-                    return coef
-
+                    output = coef
+                    break
+        if output is None:
+            raise ZernikeCoefficientsError('the index: %s is not yet Implemented, the maximum Fring index implemented is %f' % (item, self.max_fringe_order))
+        return output
+            
     def illustrate(self, max_radius=5, max_azimuth=6):
+        """
+        Provide a nice visual tool to understand how the different Zernike coefficients represent optical aberrations.
+        Example of usage:
+        a = zp.ZernikeCoefficients(33)
+        a.illustrate(5, 6)
+
+        Args:
+            max_radius (int, optional) Defaults to 5.
+            max_azimuth (int, optional) Defaults to 6.
+        """
         len_rows = max_radius + 1
         len_columns = 2 * max_azimuth + 1
         fig = plt.figure(figsize=(len_rows, len_columns))
@@ -139,7 +220,7 @@ class ZernikeCoefficients():
                 if self[(radius, azimuth)] is None:
                     continue
                 i = np.where(col == azimuth)[0]
-                x_axis, img = self[(radius, azimuth)].cartesian_matrix()
+                _, img = self[(radius, azimuth)].cartesian_matrix()
                 ax = fig.add_subplot(len_rows, len_columns, int(radius * len_columns + i + 1))
                 ax.set_title('Fringe: %s' % self[(radius, azimuth)].fringe)
                 ax.imshow(img)
@@ -156,6 +237,14 @@ class ZernikeCoefficients():
 
     @property
     def get_supported_zernike(self):
+        """
+        We support the 33 first Fringe indexed Zernike coefficient, as we want to normalize them.
+        For this we calculate the normalization (float) factors, considering the L2 norm on the unit disk should be equal to pi.
+        (integral over rho[0, 1] and theta[0, 2*pi] of (Z**2)*rho = pi)
+
+        Returns:
+            tuple: a set of Zernike Coefficients ordered in Fringe index representation.
+        """
         return (ZernikeCoefficient(1, "Piston", 1),
                 ZernikeCoefficient(2, "Tilt at 0°", 2),
                 ZernikeCoefficient(3, "Tilt at 90°", 2),
--- tests/test_optical_system/test_zernike_polynomial.py
+++ tests/test_optical_system/test_zernike_polynomial.py
@@ -33,8 +33,21 @@ def test_calculate():
     assert zernike_co.calculate_radial_polynomial(np.asarray(_rho)) == 2 * _rho ** 2 - 1
 
     zernike_co = zernike_cos[5]
-    manual_calculation = _rho ** 2 * np.cos(2 * _theta) / zernike_co.normalization
+    manual_calculation = _rho ** 2 * np.cos(2 * _theta) * zernike_co.normalization
     assert zernike_co.calculate_zernike_polynomial(np.asarray(_rho), np.asarray(_theta)) == manual_calculation
+
+
+def test_normalization():
+    zernike_cos = zp.ZernikeCoefficients(6)
+    zernike_co = zernike_cos[5]
+    
+    r = np.linspace(0, 1, 1000)
+    theta =  x = np.linspace(0, 2 * np.pi, 1000)
+    [r2, theta2] = np.meshgrid(r, theta)
+    
+    zernike_values = zernike_co.calculate_zernike_polynomial(r2, theta2)
+    l2_norm = np.sum(zernike_values ** 2 * r2) / r2.size * 2 * np.pi
+    assert l2_norm == pytest.approx(np.pi, abs=1e-2)
 
 
 def test_init():
